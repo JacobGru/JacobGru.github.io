a9e23e5c9f83303ae4a639fddd95500b77d01cf5
JacobGru <jmgruszewski@gmail.com>
2025-10-16 11:45:00 -0400

    Add files via upload

---
+++ projects/js/chapter2/js02_txt.js
@@ -0,0 +1,12 @@
+/*    JavaScript 7th Edition
+      Chapter 2
+      Chapter case
+
+      Fan Trick Fine Art Photography
+      Variables and functions
+      Author: Jacob
+      Date:   10/15/25
+
+      Filename: js02.js
+ */
+
